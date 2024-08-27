1e6ccefec2d3e90538cc02ebf3526b46fb80cb3f
rarimon <mdrimonislam221@gmail.com>
2024-08-27 22:02:06 +0600

    use string

---
+++ main.js
@@ -0,0 +1,8 @@
+let a="amar sunarSDFSFFSF bangla ami tumai valobashir";
+let c="    rimon           fghfghfghfghfhf";
+
+// let b=a.toUpperCase();
+// let b=a.concat(" " ,c);
+let b=c.trim();
+
+document.write(c);
