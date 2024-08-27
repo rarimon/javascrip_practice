096570e7c728e4f9fda4f2775a60a0ab208dceca
rarimon <mdrimonislam221@gmail.com>
2024-08-27 22:33:24 +0600

    string method

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-let a="amar sunarSDFSFFSF bangla ami tumai valobashir";
-let c="    rimon           fghfghfghfghfhf";
+// indexof
+// let a="amar sunar bangla 'aminul' ami tumai valo";
 
-// let b=a.toUpperCase();
-// let b=a.concat(" " ,c);
-let b=c.trim();
+// let b=a.indexOf("aminul");
 
-document.write(c);
+// document.write(b);
+
+// lastIndexOf
+// let a="amar sunar bangla";
+
+// let b=a.lastIndexOf("bangla");
+// let c=a.length;
+
+// document.write(b);
+// document.write(c);
+
+// let a="amar suanr bangla ami tumai";
+
+// let b=a.search("bangla");
+
+// document.write(b);
+
+// match method
+
+// let a="amar suanr bangla ami tumai valobashi";
+
+// let b=a.match("ami tumai");
+
+
+// document.write(b);
+// math all
+
+let a="amar sunar bangla ami . tumar ami valo sunar ";
+
+let b=a.endsWith("amar");
+
+document.write(b);
