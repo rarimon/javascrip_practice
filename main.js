44285b22e4817f4971a90eb8311fd325ad6e5abe
rarimon <mdrimonislam221@gmail.com>
2024-08-28 22:01:14 +0600

    array use

---
+++ main.js
@@ -0,0 +1,22 @@
+// const a=['ami','rimon','amar '];
+
+// const a=[];
+
+// a[0]="amam";
+
+// document.write(a[0]);
+
+// create new keyword an array
+
+
+// const a=['ami','tumi','amra','hello'];
+
+//  let r=a.length;
+
+//  for(i=0;i<r;i++){
+//      document.write(a[i]+" ");
+
+//  }
+
+
+
