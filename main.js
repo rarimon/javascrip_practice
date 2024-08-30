ff37483fa95fb4883a7563f2bd6d106ad2d2cc31
rarimon <mdrimonislam221@gmail.com>
2024-08-30 20:03:24 +0600

    use array

--- main.js
+++ main.js
@@ -18,5 +18,68 @@
 
 //  }
 
+// const a=['ami','rimon','akhi','mim','sumaiya'];
+
+// let b=a.forEach(aa);
+
+// function aa(bb){
+    
+//     document.write(bb);
+// }
+
+// const a=['amin','rimon'];
+
+// document.getElementById("p").innerHTML=a;
 
 
+// function show(){
+//  a.push("mim");
+//   document.getElementById("p").innerHTML=a;
+// }
+
+// show();
+
+// const a=['a','b','c'];
+
+// // let b=typeof a;
+
+// let b=Array.isArray(a);
+
+// document.write(b);
+
+// nasted array
+// join
+// const a=['rimon','akhi','mim','sumaiya'];
+
+// let b=a.join("+");
+
+// document.getElementById('p').innerHTML=b;
+
+// Pop()
+
+// const a=['hki','b','d'];
+
+
+// document.getElementById("p").innerHTML=a;
+
+// let b=a.pop();
+// document.getElementById("p1").innerHTML=a;
+// indexof
+// const a=['akhi','rimon','mim'];
+
+// let b=a.indexOf('akhi');
+
+// document.write(b);
+
+
+// const a=['akhi','rimon','mim'];
+
+// let b=a.lastIndexOf('mim');
+
+// document.write(b);
+
+const a=['akhi','rimon','mim'];
+
+let b=a.lastIndexOf('mim');
+
+document.write(b);
