c09faf098a25482825364f25f851384538de35da
rarimon <mdrimonislam221@gmail.com>
2024-08-31 23:05:22 +0600

    use

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
+// let a=Math.SQRT2;
 
-const date= new Date(2023-03-25);
+// document.write(a);
 
-let d=date.getMonth();
+//ROUND
+// let a=Math.round(4.5);
+
+// let a=Math.ceil(4.1);
+// ternary operator
+
+// function myf(){
+//     let age=document.getElementById("age").value;
+//     let result=(age<18)?"hell":"wow";
+//    document.getElementById("p").innerHTML=result;
+// }
 
 
-document.getElementById('p').innerHTML=d;
+// document.write(a);
+
+// for in loop
+
+// const a={
+// name:"aminul",
+// age:20,
+// color:"red"
+// }
+
+// let b="";
+
+// for(let x in a){
+//  b+=a[x]+" ";
+//  document.getElementById("p").innerHTML=b;
+// }
+
+// const a=[435,63,,23,747,6546,46];
+
+// for(let x in a){
+// document.write(a[x]+"<br>");
+// }
+
+let a=[22,242,234,242,45,5,45,35];
+let b=a.forEach(myf);
+
+function myf(value){
+document.write(value+" <br>");
+}
